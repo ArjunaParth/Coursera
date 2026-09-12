f3180cd59bf7651f80d0d641501941a55f5fbea6
ArjunaParth <pandeyparth752@gmail.com>
2026-09-11 22:31:25 -0700

    Add files via upload

---
+++ StudySyncTests.swift
@@ -0,0 +1,18 @@
+//
+//  StudySyncTests.swift
+//  StudySyncTests
+//
+//  Created by Parth Pandey on 18/06/26.
+//
+
+import Testing
+
+struct StudySyncTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
